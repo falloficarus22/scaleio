387fa1374f10dc6a5407ac15949bc4f20056316f
Abhishek <norizzabhii@gmail.com>
2026-04-13 07:32:45 +0000

    Completed core architecture components

--- models/backbones/swinir_l.py
+++ models/backbones/swinir_l.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from typing import Tuple, Optional
 import math
 from einops import rearrange
+import numpy as np
 
 
 class WindowAttention(nn.Module):
@@ -130,3 +131,182 @@ class WindowAttention(nn.Module):
 
         if pad_r > 0 or pad_b > 0:
             x = x[:, :H, :W, :].contiguous()
+
+        x = x.view(B, H * W, C)
+
+        x = shortcut + x
+        x = x + self.mlp(self.norm(x))
+
+        return x
+
+
+class PatchMerging(nn.Module):
+
+    def __init__(self, dim: int):
+        super().__init__()
+        self.dim = dim
+        self.reduction = nn.Linear(4 * dim, 2 * dim, bias=False)
+        self.norm = nn.LayerNorm(4 * dim)
+
+    def forward(self, x:torch.Tensor, H: int, W: int) -> torch.Tensor:
+        B, L, C = x.shape
+        assert == H * W, "input fearture has a wrong size"
+
+        x = x.view(B, H, W, C)
+        pad_input = (H % 2 == 1) or (W % 2 == 1)
+        if pad_input:
+            x = F.pad(x, (0, 0, 0, W % 2, 0, H % 2))
+
+        x0 = x[:, 0::2, 0::2, :]
+        x1 = x[:, 1::2, 0::2, :]
+        x2 = x[:, 0::2, 1::2, :]
+        x3 = x[:, 1::2, 1::2, :]
+        x = torch.cat([x0, x1, x2, x3], -1)
+
+        x = x.view(B, -1, 4 * C)
+        x = self.norm(x)
+        x = self.reduction(x)
+
+        return x
+
+
+class asicLayer(nn.Module):
+
+    def __init__(self, dim: int, depth: int, num_heads: int, window_size: Tuple[int, int] = 7,
+        mlp_ratio: float = 4.0, drop: float = 0.0):
+        super().__init__()
+        self.dim = dim
+        self.depth = depth
+
+        self.blocks = nn.ModuleList([
+            SwinTransformerBlock(
+                dim=dim,
+                num_heads=num_heads,
+                window_size=window_size,
+                shift_size=(0, 0) if (i % 2 == 0) elsse (window_size[0] // 2, window_size[1] // 2),
+                mlp_ratio=mlp_ratio,
+                drop=drop
+            )
+            for i in range(depth)
+        ])
+
+        if depth != 1:
+            self.patch_merge = PatchMerging(dim)
+        else:
+            self.patch.merge = None
+
+    def forward(self, x: torch.Tensor, H: int, W: int) -> Tuple[torch.Tensor, int, int]:
+        Hp = int(np.cell(H / self.window_size[0])) * self.window_size[0]
+        Wp = int(np.cell(W / self.window_size[1])) * self.window_size[1]
+        img_mask = torch.zeros((1, Hp, Wp, 1), device=x.device)
+        h_slices = (slice(0, -self.window_size[0]),
+            slice(-self.window_size[0], -self.shift_size[0]),
+            slice(-self.shift_size[0], None))
+        w_slices = (slice(0, -self.window_size[1]),
+            slice(-self.window_size[1], -self.shift_size[1]),
+            slice(-self.shift_size[1], None))
+
+        cnt=0
+        for h in h_slices:
+            for w in w_slices:
+                img_mask[:, h, w, :] = cnt
+                cnt += 1
+
+        mask_windows = window_partition(img_mask, self.window_size)
+        mask_windows = mask_window.view(-1, self.window_size[0] * self.window_size[1])
+
+        attn_mask = mask_windows.unsqueeze(1) - mask_windows.unsqueeze(2)
+        attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))
+
+        for blk in self.blocks:
+            blk.H, self.W = H, W
+            x = blk(x, attn_mask)
+
+        if self.patch_merge is not None:
+            H, W = (H + 1) // 2, (W + 1) // 2
+            x = self.patch_merge(x, H, W)
+
+        return x, H, W
+
+
+class SwinIRL(nn.Module):
+
+    def __init__(self,
+        img_size: int = 64,
+        patch_size: int = 1,
+        in_channs: int = 3,
+        embed_dim: int = 180,
+        depths: Tuple[int, ...] = (6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6),
+        num_heads: Tuple[int, ...] = (6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6),
+        window_size: int = 7,
+        mlp_ratio: float = 4.0,
+        drop_ratio: float = 0.0):
+        super().__init__()
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.in_channs = in_channs
+        self.embed_dim = embed_dim
+
+        self.patch_embed = nn.Conv2d(in_channs, embed_dim, kernel_size=patch_size, stride=patch_size)
+
+        self.layers = nn.ModuleList()
+        for i_layer in range(len(depths)):
+            layer = BasicLayer(
+                dim=int(embed_dim * 2 ** (i_layer % 4)),
+                num_heads=num_heads[i_layer],
+                window_size=window_size,
+                mlp_ratio=mlp_ratio,
+                drop=drop_rate
+            )
+            self.layers.append(layer)
+
+        self.norm = nn.LayerNorm(embed_dim)
+        self.patch_unembed = nn.Conv2d(embed_dim, 3 * patch_size ** 2, kernel_size=1)
+        self.pixel_shuffle = nn.PixelShuffle(patch_size)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=0.2)
+            if m.bias is not None:
+                nn.init.constant_(m_bias, 0)
+
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, C, H, W = x.shape
+
+        x = self.patch_embed(x)
+        x = x.flatten(2).transpose(1, 2)
+
+        for layer in self.layers:
+            x, H, W = layer(x, H, W)
+
+        x = self.norm(x)
+        x = x.transpose(1, 2).reshape(B, self.embed_dim, H, W)
+        x = self.patch_unembed(x)
+        x = self.pixel_shuffle(x)
+
+        return x
+
+def trunc_normal(tensor, mean=0., std=1., a=-2., b=2.):
+    with torch.no_grad():
+        tensor.normal_(mean, std)
+        tensor.clamp_(min=a, max=b)
+
+def window_partition(x: torch.Tensor, window_size: Tuple[int, int]) -> torch.Tensor:
+    B, H, W, C = x.shape
+    x = x.view(B, H // window_size[0], window_size[0], W // window_size[1], window_size[1], c)
+    windows = x.permute(0, 1, 3, 2, 4, 5).continguous()
+    windows = window.view(-1, window_size[0] * window[1], C)
+    return windows
+
+def window_reverse(windows: torch.Tensor, window_size: Tuple[int, int], H: int, W: int) -> torch.Tensor:
+    B = int(windows.shape[0] / (H * W / window_size[0] / window_size[1]))
+    x = windows.view(B, H // window_size[0], W // window_size[1], window_size[0], window_size[1], -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).continguous().view(B, H, W, -1)
+
+    return x
